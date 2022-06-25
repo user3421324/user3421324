14c90e9d829caa6c3188d0f28c3a753000d74432
user3421324 <107748245+user3421324@users.noreply.github.com>
2022-06-25 11:42:35 +0300

    Add files via upload

--- Пинг-Понг.py
+++ Пинг-Понг.py
@@ -19,25 +19,47 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
     def update_r(self):
-       keys = key.get_pressed()
+        keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x >5:
             self.rect.x -= self.speed
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
     def update_l(self):
-       keys = key.get_pressed()
+        keys = key.get_pressed()
         if keys[K_w] and self.rect.x >5:
             self.rect.x -= self.speed
         if keys[K_s] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
+game = True
+finish = False
 win_height = 500
 win_width = 700       
+
 FPS = 60
-racket1 = Player("racket.png", 30, 200, 4, 50, 150)
-racket2 = Player("racket.png", 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png.png', 200, 200, 4, 50, 50)
-display.set_caption('')
-display.update()
+racket1 = Player("racket.png.png", 30, 200, 20, 50, 150)
+racket2 = Player("racket.png.png", 520, 200, 20, 50, 150)
+ball = GameSprite('ball.png.png', 200, 200, 70, 50, 50)
+color_fon = (200, 150, 0)
+clock = time.Clock()
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish == False:
+        window.fill(color_fon)
+        racket1.reset()
+        racket1.update_l()
+        racket2.reset()
+        racket2.update_r()
+        ball.reset()
+        ball.update()
+        
+    
+    
+
+
+
+    display.update()
     clock.tick(FPS)
 
 
